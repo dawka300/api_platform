bee78ef5bd848864c679cc642ef27c8656649775
dwesolowski <dawid.wesolowski@verestro.com>
2021-03-21 15:35:20 +0100

    add an entity and migration

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -13,16 +15,57 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class BlogController extends AbstractController
 {
-    public function list() {
+    private const POSTS = [
+        [
+            'id' => 1,
+            'slug' => 'slug',
+            'title' => 'Hello Word'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'another-post',
+            'title' => 'Another Post'
+        ],
+        [
+            'id' => 3,
+            'slug' => 'last-example',
+            'title' => 'Last example'
+        ]
+    ];
 
+    /**
+     * @Route("/{page}", name="blog_list", defaults={"page": 1})
+     * @param $page
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function list($page, Request $request) {
+        $limit = $request->get('limit', 10);
+      return  $this->json([
+          'page' => $page,
+          'limit' => $limit,
+          'data' => array_map(function ($item){
+              return $this->generateUrl('blog_by_slug', ['slug' => $item['slug']]);
+          }, self::POSTS)
+      ]);
     }
 
-    public function post(){
-
+    /**
+     * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     */
+    public function post($id){
+       return $this->json(
+           self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+       );
     }
 
-    public function postBySlug() {
-
+    /**
+     * @Route("/{slug}", name="blog_by_slug")
+     */
+    public function postBySlug($slug) {
+       return $this->json(
+           self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+       );
     }
 
 }
