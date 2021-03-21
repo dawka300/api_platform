edef4584b15712a53e56c850869392c86c586cde
dwesolowski <dawid.wesolowski@verestro.com>
2021-03-21 14:40:58 +0100

    add new controllers

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class BlogController
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+    public function list() {
+
+    }
+
+    public function post(){
+
+    }
+
+    public function postBySlug() {
+
+    }
+
+}
